cf2bd84714b270843b80e3f176591cfa892247af
tomas-rln <tomas.ezequiel.rln@gmail.com>
2021-07-21 22:42:39 -0300

    Dotenv configuration. Home page added. Plugin runtime added

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Personal Challenge Alkemy 
+## Installation: 
+First clone this repo: 
+```http
+    git clone https://github.com/rln-tomas/personal-ch-front
+```
+Rename the file '.env.example' to '.env': 
+```bash
+    mv .env.example .env 
+```
+Install the dependencies: 
+```bash
+    npm install
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 module.exports = {
   output: {
     filename: 'app.bundle.js'
@@ -6,7 +7,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
-    })
+    }),
+    new Dotenv() // For environments variables
   ],
   module: {
     rules: [
@@ -19,7 +21,8 @@ module.exports = {
             presets: [
               '@babel/preset-env',
               '@babel/preset-react'
-            ]
+            ],
+            plugins: ['@babel/plugin-transform-runtime'] // For redux
           }
         }
       }
