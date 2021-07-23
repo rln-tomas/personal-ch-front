947fdda1b9102b845bd81aa9e98adbd718089599
tomas-rln <tomas.ezequiel.rln@gmail.com>
2021-07-23 11:51:57 -0300

    Background for home page.

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,6 +2,6 @@ import { makeStyles } from '@material-ui/core'
 
 export const useStyles = makeStyles(theme => ({
   homeContainer: {
-    backgroundColor: '#fff'
+    background: 'linear-gradient(90deg, #2196F3 20%, #21CBF3 100%)'
   }
 }))
